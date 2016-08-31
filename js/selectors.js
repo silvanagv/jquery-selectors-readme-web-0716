898959efa3574840b95b3ad547502c22786b188e
Silvana Goberdhan-Vigle <silvanagoberdhanvigle@gmail.com>
2016-08-31 16:27:00 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+// $('#baby-ninja').
+
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function firstListItem() {
+  return $('ul li:first')
+}
+
+function divSelector() {
+  return $('div.pics')
+}
